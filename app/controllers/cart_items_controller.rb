804b7b75732b046224c90637f7d56179ebddc867
EC2 Default User <ec2-user@ip-172-31-76-44.ec2.internal>
2020-12-15 06:30:57 +0000

    まだできてません(注文)

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,23 @@
-class CartItemsController < ApplicationController
+class public::CartItemsController < ApplicationController
+  
+#カート内商品一覧
+def index
+end
+
+#カート内商品追加
+def create
+end
+
+#カート内商品個数変更
+def update
+end
+
+#カート内商品削除
+def destroy
+end
+
+#カートをからにする
+def destroy_all
+end
+
 end
