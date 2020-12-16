5100d127c022a5c8f80517eb1b234282f853e065
mnwhryk <minomushi.ff15.07.08@gmail.com>
2020-12-16 11:46:47 +0000

    shipping-view

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,24 @@
 class Public::CustomersController < ApplicationController
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(
+      :email,
+      :family_name,
+      :first_name,
+      :kana_family_name,
+      :kana_first_name,
+      :zipcode,
+      :address,
+      :phone_number
+    )
+  end
 end
