d12ae707d8ce9d9c466ed5ea6c6742be28ff3ad0
EC2 Default User <ec2-user@ip-172-31-76-44.ec2.internal>
2020-12-17 09:58:59 +0000

    add注文機能入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,8 @@
 class OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+ 
+ def confirm
+ end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,9 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
 
+
+enum payment:{
+  bank: 0,
+  credit: 1
+} 
 end
