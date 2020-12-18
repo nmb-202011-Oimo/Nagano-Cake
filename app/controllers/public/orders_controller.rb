0525b792e91d4ade030c085d78a4a64906d06c23
EC2 Default User <ec2-user@ip-172-31-76-44.ec2.internal>
2020-12-18 05:15:36 +0000

    注文機能(仮)

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,5 +4,16 @@ class OrdersController < ApplicationController
   end
  
  def confirm
+   @order = Order.new #<=一応newで作ってみたが必要かは不明
+   @cart_items = current_customer.cart_items #<=ユーザーのカート内商品をインスタンスに格納
+   @shipping_fee = 800  #<=送料、いつかここの800もモデルに記入してadmin側で変更したい
+   @order_payment = params[:order][:payment]
+   if params[:order][:address_option] == "0"
+     @order_address = current_customer.shipping #<=強引に入れた、多分成功しない
+    elsif params[:order][:address_option] == "1"
+      @order_address = Shipping.find(params[:zipcode], [:address], [:name]) #<=上に同じくだいぶ強引に入れた多分無理
+    else
+      
+   end
  end
 end
