4fc9c9ee371fb10b7202db5310b9bfa7b39c33fe
74905 <hurandall@yahoo.co.jp>
2020-12-18 06:39:14 +0000

    seed追加途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+     
+     
+     
+     def configure_permitted_parameters
+      if resource_class == Customer
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name,:first_name,:kana_family_name,:kana_first_name,:zipcode,:address,:phone_number,:email])
+            devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
+            devise_parameter_sanitizer.permit(:account_update,keys:[:name,:email])
+      end
+    end
 end
