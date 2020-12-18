5dcf0d8bfbb16a4d297d116da9b7c0f262518fc0
74905 <hurandall@yahoo.co.jp>
2020-12-18 06:37:55 +0000

    seed追加途中

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,56 @@
 
 Admin.create!(
    email: 'test@test.com',
-   password: 'testtest',
+   encrypted_password: 'testtest'
 )
+
+Customer.create!(
+   family_name: "田中",
+   first_name: "英一",
+   kana_family_name: "タナカ",
+   kana_first_name: "ヒデカズ",
+   email: "nagano@cake.com",
+   encrypted_password: "123456",
+   zipcode: "0000000",
+   address: "大阪府箕面市小野原東",
+   phone_number: "12345678901",
+   )
+
+Category.create!(
+   name: "ケーキ",
+   is_active: true,
+   )
+
+Category.create!(
+   name: "チョコレート",
+   is_active: true,
+   )
+
+Category.create!(
+   name: "クッキー",
+   is_active: true,
+   )
+
+Item.create!(
+   category_id: 1,
+   name: "ショートケーキ",
+   price: 400,
+   introduction: "いちごの入ったケーキです",
+   image_id: nil,
+   is_active: true,)
+
+Item.create!(
+   category_id: 1,
+   name: "チョコケーキ",
+   price: 400,
+   introduction: "チョコの入ったケーキです",
+   image_id: nil,
+   is_active: true,)
+   
+Item.create!(
+   category_id: 1,
+   name: "モンブランケーキ",
+   price: 400,
+   introduction: "栗の入ったケーキです",
+   image_id: nil,
+   is_active: true,)
