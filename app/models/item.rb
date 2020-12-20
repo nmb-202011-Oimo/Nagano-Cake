2c6ebf0fb1c0b1391c8d7342654de4182fbefe52
mnwhryk <minomushi.ff15.07.08@gmail.com>
2020-12-20 07:29:57 +0000

    validates-production

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,12 @@ class Item < ApplicationRecord
 
   belongs_to :category
   has_many :order_details, dependent: :destroy
-  has_many :customers, through: :cart_items　
+  has_many :customers, through: :cart_items
   # order_detailsも中間テーブルだと思ったので、新規で追加しました
   has_many :orders, through: :order_details
   has_many :cart_items
 
-
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates :introduction, presence: true
 end
--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -2,4 +2,8 @@ class Shipping < ApplicationRecord
 
   belongs_to :customer
 
+  validates :name, presence: true, uniqueness: true
+  validates :zipcode, presence: true
+  validates :address, presence: true
+
 end
