8de71707c02fc7ec86b10740cce9ddc15ac4ea3b
corogit <n.ohta5192@gmail.com>
2020-12-22 02:31:57 +0000

    admin-final

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,3 +1,22 @@
-class OrderDetailsController < ApplicationController
+class Admin::OrderDetailsController < ApplicationController
 before_action :authenticate_admin!
+
+    def update 
+        @orders = Order.all
+        @order_detail = OrderDetail.find(params[:id])
+        if @order_detail.update(order_detail_params)
+            if @order_detail.product_status == "製作中"
+                @orders.update_all(status: "製作中" )
+                flash[:success] = "製作ステータスを変更しました"
+		        redirect_to admin_orders_path(@order_detail.order)
+		    end
+        else
+	        render "order/show"
+        end
+    end
+    
+    private
+    def order_detail_params
+        params.require(:order_detail).permit(:product_status)
+    end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,13 +7,18 @@ class Admin::OrdersController < ApplicationController
     
     def show
         @order = Order.find(params[:id])
+        @order_details = @order.order_details
     end
     
     def update
         @order = Order.find(params[:id])
+        @order_details = @order.order_details
         if @order.update(order_params)
-          flash[:success] = "注文ステータスを変更しました"
-          redirect_to admin_order_path(@order)
+            if @order.status == "入金確認"
+                @order_details.update_all(product_status: 1 )
+                flash[:success] = "注文ステータスを変更しました"
+                redirect_to admin_order_path(@order.id)
+            end
         else
             render :show
         end
